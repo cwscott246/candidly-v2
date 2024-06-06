b5564dcf9b44ea8bd185779a71b3e4581f1e9b5a
Charlie Scott <cwscott246@gmail.com>
2024-06-06 14:09:10 -0400

    initial

---
+++ src/main/java/com/example/candidlyv2/CandidlyV2Application.java
@@ -0,0 +1,13 @@
+package com.example.candidlyv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidlyV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandidlyV2Application.class, args);
+    }
+
+}
